dd5c6f5c1ec9f716efaef943616b266c72e630f0
enl001 <enliven001@mail.ru>
2020-04-12 23:53:51 +0300

    feat: create card template

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import vocabulary from '@data/vocabulary';
 import { sourceType, getPath } from '@utils/path';
 
 // https://www.youtube.com/watch?v=eSaF8NXeNsA&vl=en-GB
+// https://javascript.info/custom-elements
+// https://ayushgp.github.io/html-web-components-using-vanilla-js/
 
 const { categories } = vocabulary;
 console.log(categories);
@@ -12,4 +14,18 @@ document.addEventListener('click', () => {
   const soundPath = getPath(sourceType.sound, set[1].pronunciation);
   const audio = new Audio(soundPath);
   audio.play();
+});
+
+const card = document.getElementById('card');
+
+card.addEventListener('click', (e) => {
+  e.stopPropagation();
+  if (card.classList.contains('card_flip')) {
+    card.classList.add('card_unflip');
+    setTimeout(() => {
+      card.classList.remove('card_flip', 'card_unflip');
+    }, 500);
+  } else {
+    card.classList.add('card_flip');
+  }
 });
